bfc84d1db63d6146468816dfba061cbe1d872463
Trecia Kat <tksekgalo@gmail.com>
2023-01-21 17:26:57 +0200

    removed test code

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Container from "./components/Container";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
     <Container />
     </>
   );
