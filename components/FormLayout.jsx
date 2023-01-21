7b8a7db588abaef733b5002dd2a7a9f370ecc39d
Trecia Kat <tksekgalo@gmail.com>
2023-01-21 17:10:10 +0200

    styled form layout

---
+++ components/FormLayout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const FormLayout = ({children}) => {
+  return (
+    <section className="body-font relative lg:mt-8">
+        <div className="container mx-auto px-5 py-12">
+            <div className="mx-auto w-10/12">
+                <div class="mb-6">
+                    <h1 class="text-3xl font-semibold text-gray-800 capitalize">
+                        Business Sign Up
+                    </h1>
+
+                    <p class="mt-1 text-gray-500 tracking-wider">
+                        Fill in the fields below to get started
+                    </p>
+                </div>
+                {children}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default FormLayout
