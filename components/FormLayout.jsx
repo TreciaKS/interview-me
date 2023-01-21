40f535d4d0640e022fa4bc80b05e7af25790419e
Trecia Kat <tksekgalo@gmail.com>
2023-01-21 17:02:24 +0200

    Styled form layout

---
+++ components/FormLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FormLayout = ({children}) => {
+  return (
+    <>
+        <main className="bg-white">
+            <div className="flex justify-center min-h-screen">
+                <header className="w-full px-12 py-8 bg-white">
+                    {children}
+                </header>
+
+            {/* place images */}
+            <div class="bg-cover lg:block lg:w-1/2">
+            </div>
+
+            </div>
+        </main>
+    </>
+  )
+}
+
+export default FormLayout
