cdc3d526fcf823e81388c42f8f80482b13f08f16
Trecia Kat <tksekgalo@gmail.com>
2023-01-22 13:50:37 +0200

    using routes in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
-
+import { Route, Routes } from "react-router-dom";
 import Container from "./components/Container";
+import EmailSubmission from './components/EmailSubmission'
 
 function App() {
   return (
     <>
-    <Container />
+      <Routes>
+        <Route path="/" element={<Container />} />
+        <Route path="/EmailSubmission" element={<EmailSubmission />} />
+      </Routes>
     </>
   );
 }
